ebb20629a6fab73b148911d5bf4290a7b9283013
helloworldRK <110299413+helloworldRK@users.noreply.github.com>
2022-07-31 15:40:01 +0300

    your msg

---
+++ src/hca.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class hca {
+    public static void main(String[] args) {
+
+
+        int a, b;
+        double c;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("1.kenar");
+        a = inp.nextInt();
+        System.out.println("2.kenar");
+        b = inp.nextInt();
+        c = Math.sqrt((a * a) + (b * b));
+        System.out.println("Hipotenus:"+c);
+
+
+    }
+}
