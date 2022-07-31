895f99c894073a68fdd951a14e4d555fb0600d69
helloworldRK <110299413+helloworldRK@users.noreply.github.com>
2022-07-31 18:15:26 +0300

    commit message

--- src/taks.java
+++ src/taks.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class taks {
+import java.util.Scanner;
+public class taks {
+    public static void main(String []args ){
+        int km;
+        double perKm=2.20, total=10;
+        Scanner inp= new Scanner(System.in);
+        System.out.println("Gidilen mesafeyi KM cinsinden Giriniz: ");
+        km=inp.nextInt();
+        total+=(km+perKm);
+        total=(total<20)? 20:total;
+        System.out.println("Toplam Tutar: "+total);
+    }
 }
